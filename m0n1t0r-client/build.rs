0fb6084b5efd6074c47436d32918ca92e7c04f68
MMitsuha <miyammitsuha@gmail.com>
2024-11-13 02:04:12 +0800

    Refactor feature flags for cross-platform support and update build script for Windows integration

--- m0n1t0r-client/build.rs
+++ m0n1t0r-client/build.rs
@@ -1,12 +1,19 @@
 use std::path::PathBuf;
 
-const PROJECT_LIST: [&str; 1] = ["m0n1t0r-cpp-windows-lib"];
+const PROJECT_LIST_WINDOWS: [&str; 1] = ["m0n1t0r-cpp-windows-lib"];
 
 fn xmake_build() -> Vec<PathBuf> {
-    PROJECT_LIST
-        .iter()
-        .map(|x| xmake::build(format!("{}", x)))
-        .collect::<Vec<PathBuf>>()
+    let mut paths = Vec::new();
+
+    #[cfg(feature = "windows")]
+    paths.append(
+        &mut PROJECT_LIST_WINDOWS
+            .iter()
+            .map(|x| xmake::build(format!("{}", x)))
+            .collect::<Vec<PathBuf>>(),
+    );
+
+    paths
 }
 
 fn main() {
@@ -14,7 +21,8 @@ fn main() {
         println!("cargo:rustc-link-search=native={}", path.display());
     }
 
-    PROJECT_LIST.iter().for_each(|x| {
+    #[cfg(feature = "windows")]
+    PROJECT_LIST_WINDOWS.iter().for_each(|x| {
         println!("cargo:rustc-link-lib={}", x);
     });
 }
