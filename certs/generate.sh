aa68f2bab289c1c6756c097f195b7e7f80f29263
MMitsuha <miyammitsuha@gmail.com>
2024-11-21 04:51:26 +0800

    feat: add subject alternative name configuration for certificate extensions

--- certs/generate.sh
+++ certs/generate.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-openssl req -x509 -sha512 -days 3650 -newkey rsa:4096 -keyout ca.key -out ca.crt -nodes -subj "/C=CN/ST=ShangHai/L=ShangHai/O=K and A Ltd/OU=./CN=mmitsuha.xyz"
+openssl req -x509 -sha512 -days 3650 -newkey rsa:4096 -keyout ca.key -out ca.crt -nodes -subj "/C=CN/ST=ShangHai/L=ShangHai/O=K and A Ltd/OU=./CN=mmitsuha.xyz."
 openssl genpkey -algorithm RSA -out end.key -pkeyopt rsa_keygen_bits:4096
-openssl req -new -key end.key -out end.csr -subj "/C=CN/ST=ShangHai/L=ShangHai/O=K and A Ltd/OU=./CN=mmitsuha.xyz"
+openssl req -new -key end.key -out end.csr -subj "/C=CN/ST=ShangHai/L=ShangHai/O=K and A Ltd/OU=./CN=mmitsuha.xyz."
 openssl x509 -req -in end.csr -CA ca.crt -CAkey ca.key -CAcreateserial -extfile cert.ext -out end.crt -days 3650 -sha256
 openssl rsa -in ca.key -out ca.key.der -outform DER
 openssl x509 -in ca.crt -out ca.crt.der -outform DER
