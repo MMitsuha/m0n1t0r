bd557386999f1297104bacf5c039156a878724f2
MMitsuha <miyammitsuha@gmail.com>
2024-11-21 21:20:25 +0800

    fix: Build on windows

--- m0n1t0r-common/src/screen/mod.rs
+++ m0n1t0r-common/src/screen/mod.rs
@@ -3,21 +3,52 @@ use remoc::{
     rch::bin::{self, Receiver},
     rtc,
 };
-use scap::capturer::{Capturer, Options};
+use scap::{
+    capturer::{Area, Capturer, Resolution},
+    frame::FrameType,
+};
+use serde::{Deserialize, Serialize};
+use std::thread;
+use tokio::runtime::Builder;
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
+pub struct Options {
+    pub fps: u32,
+    pub show_cursor: bool,
+    pub show_highlight: bool,
+    pub crop_area: Option<Area>,
+    pub output_type: FrameType,
+    pub output_resolution: Resolution,
+}
+
+impl Options {
+    pub fn into_scap(self) -> scap::capturer::Options {
+        scap::capturer::Options {
+            fps: self.fps,
+            show_cursor: self.show_cursor,
+            show_highlight: self.show_highlight,
+            crop_area: self.crop_area,
+            output_type: self.output_type,
+            output_resolution: self.output_resolution,
+            ..Default::default()
+        }
+    }
+}
 
 #[rtc::remote]
 pub trait Agent: Sync {
     async fn record(&self, options: Options) -> AppResult<Receiver> {
         let (tx, remote_rx) = bin::channel();
 
-        tokio::spawn(async move {
-            let mut recorder = Capturer::build(options)?;
-            let mut tx = tx.into_inner().await?;
+        thread::spawn(move || {
+            let runtime = Builder::new_current_thread().enable_all().build()?;
+            let mut recorder = Capturer::build(options.into_scap())?;
+            let mut tx = runtime.block_on(tx.into_inner())?;
 
             recorder.start_capture();
             loop {
                 let frame = recorder.get_next_frame()?;
-                tx.send(rmp_serde::to_vec(&frame)?.into()).await?;
+                runtime.block_on(tx.send(rmp_serde::to_vec(&frame)?.into()))?;
             }
 
             #[allow(unreachable_code)]
