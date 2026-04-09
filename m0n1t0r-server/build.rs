96406101445985d4b42f89b8500faa8530fa19ed
Miyamizu Mitsuha <miyammitsuha@gmail.com>
2026-04-10 01:04:28 +0800

    refactor: server build.rs uses m0n1t0r-build platform/winres modules

--- m0n1t0r-server/build.rs
+++ m0n1t0r-server/build.rs
@@ -1,31 +1,11 @@
-#[cfg(not(any(
-    feature = "winnt",
-    feature = "linux",
-    feature = "macos",
-    feature = "general"
-)))]
-compile_error!("No target platform specified.");
-
-#[cfg(feature = "winnt")]
-fn add_manifest_windows() {
-    use std::path::Path;
-    use winapi::um::winnt;
-
-    let mut res = winres::WindowsResource::new();
-    res.set_icon(
-        Path::new(env!("CARGO_WORKSPACE_DIR"))
-            .join("resource/ms.ico")
-            .to_str()
-            .unwrap(),
-    )
-    .set_language(winnt::MAKELANGID(
-        winnt::LANG_ENGLISH,
-        winnt::SUBLANG_ENGLISH_US,
-    ));
-    res.compile().unwrap();
-}
+use std::path::Path;
 
 fn main() {
+    m0n1t0r_build::platform::ensure();
+
     #[cfg(feature = "winnt")]
-    add_manifest_windows();
+    m0n1t0r_build::winres::embed(
+        &Path::new(env!("CARGO_WORKSPACE_DIR")).join("resource/ms.ico"),
+        None,
+    );
 }
